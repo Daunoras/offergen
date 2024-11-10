01922439589f09762272a37ffc2a50297d5c9bd7
Justinas Daunoras <justdaunoras@gmail.com>
2024-11-10 05:37:21 +0200

    product

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,2 @@
 from . import offer_gen
+from . import product_product
--- models/offer_gen.py
+++ models/offer_gen.py
@@ -12,6 +12,8 @@ class OfferGen(models.Model):
         ], default='new')
     client = fields.Many2one('res.partner', string='Klientas', required=True)
     sale = fields.Many2one('sale.order', string='Pardavimas', readonly=True)
+    products = fields.Char(string='Produktai')
+    product_count = fields.Integer(string='Produktų skaičius', compute="_compute_product_count", store=False)
 
 
     @api.model
@@ -19,3 +21,6 @@ class OfferGen(models.Model):
         if vals.get('name', 'Naujas') == 'Naujas':
             vals['name'] = self.env['ir.sequence'].next_by_code('offer.generator.sequence')
         return super(OfferGen, self).create(vals)
+
+    def _compute_product_count(self):
+        return len(set(self.products.split()))
